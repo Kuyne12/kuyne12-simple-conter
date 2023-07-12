52e83ea925614f6918dcddab44de60ba89255b7e
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-07-12 04:46:12 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Digit from "./Digit";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [count, setCount] = useState(0);
+	useEffect(()=>{
+		setInterval(() => {
+			setCount(count++)
+			
+		},1000);
+
+	},[])
+	const increment = () => {
+	  setCount(count + 1);
+	};
+  
+	const decrement = () => {
+	  setCount(count - 1);
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<h1>Counter</h1>
+		<div className="digit-cont"> 
+			<Digit count={Math.floor(count/100000)}/> 
+			<Digit count={Math.floor(count/10000)}/> 
+			<Digit count={Math.floor(count/1000)}/> 
+			<Digit count={Math.floor(count/100)}/> 
+			<Digit count={Math.floor(count/10)}/> 
+			<Digit count={Math.floor(count/1)}/> 
 		</div>
+		{/* <p>Count: {count}</p>
+		<button onClick={increment}>Increment</button>
+		<button onClick={decrement}>Decrement</button> */}
+	  </div>
 	);
 };
 
